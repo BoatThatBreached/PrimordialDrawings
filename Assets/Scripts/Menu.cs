55d42ba320fadc25deed31785d715276f5749562
BoatThatBreached <miskam1345@gmail.com>
2022-04-25 17:28:30 +0500

    modified gitignore

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Exit() => Application.Quit();
+    public void Play() => SceneManager.LoadScene("GameScene");
+}
